9ade994dcc518c67d08c95d20a89eb0467802a8d
flavio silva <flavio.devjs@gmail.com>
2021-03-16 14:02:04 -0300

    cliente

--- src/components/ClientArea.jsx
+++ src/components/ClientArea.jsx
@@ -1,13 +1,68 @@
-import { Typography } from 'antd';
+import { Typography, Form, Input, Button, Checkbox, Row, Col  } from 'antd';
 
 const { Title, Paragraph, Text, Link } = Typography;
 
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 export function ClientArea() {
     return (
-        <Typography>
-            <Title>
-                Área do cliente
-            </Title>
-        </Typography>
+        <>
+            <Row>
+                <Col span={12} style={{marginTop: '8rem'}}>
+                    <Form
+                        {...layout}
+                    >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[
+                                {
+                                required: true,
+                                message: 'Please input your username!',
+                                },
+                            ]}
+                        >
+                        <Input />
+                        </Form.Item>
+                
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                required: true,
+                                message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                        <Input.Password />
+                        </Form.Item>
+                
+                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                
+                        <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </>
     )
 }
