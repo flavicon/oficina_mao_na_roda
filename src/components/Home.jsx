c7a41542277060737c158f90ecacf8c7541b6e49
flavio silva <flavio.devjs@gmail.com>
2021-03-16 13:53:47 -0300

    a empresa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
-import { Typography, Layout } from 'antd';
+import { Typography, Layout, Row, Col } from 'antd';
+
+import styles from '../styles/components/home.module.css';
 
 const { Header, Content, Footer } = Layout;
 
@@ -6,22 +8,30 @@ const { Title, Paragraph, Text, Link } = Typography;
 
 export function Home() {
     return (
-        <Layout style={{height: '100vh'}}>
-            <Header>
-                <Typography>
+        <>
+            <Row>
+                <Col span={8} offset={10}>
                     <Title>
-                        A Empresa
+                        A empresa
                     </Title>
-                </Typography>
-            </Header>
-            <Content>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam atque alias quis distinctio odio fuga ipsam est qui dicta quas, debitis vero cumque perferendis necessitatibus, voluptatem iste eligendi quaerat sint.
-            </Content>
-            <Footer>
-                <Paragraph>
-                    by: mãonaroda.
-                </Paragraph>
-            </Footer>
-        </Layout>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={12} offset={6}>
+                    <Layout style={{height: '100vh'}}>
+                        <Content>
+                            <Paragraph>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet distinctio atque pariatur, nam aliquid eligendi placeat asperiores quod doloremque tenetur minima repudiandae dolorem amet! At corporis amet aperiam adipisci quibusdam!
+                            </Paragraph>
+                        </Content>
+                        <Footer>
+                            <Paragraph>
+                                teste
+                            </Paragraph>
+                        </Footer>
+                    </Layout>
+                </Col>
+            </Row>
+        </>
     )
 }
